f5a227f7e7cd6db5f0423ebb950b4eefce6bbc9b
Viicos <victo.plot@gmail.com>
2022-09-13 11:25:22 +0200

    :sparkles: [#7] -- Add redirect view

--- src/bdeapp/siteconfig/admin.py
+++ src/bdeapp/siteconfig/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 
 from solo.admin import SingletonModelAdmin
 
-from bdeapp.siteconfig.models import SiteConfiguration
+from bdeapp.siteconfig.models import RedirectLink, SiteConfiguration
 
 
 @admin.register(SiteConfiguration)
 class SiteConfigurationAdmin(SingletonModelAdmin):
     pass
+
+@admin.register(RedirectLink)
+class RedirectLinkAdmin(admin.ModelAdmin):
+    list_display = ("url_slug", "url")
+    search_fields = ("url_slug", "url")
