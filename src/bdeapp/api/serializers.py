dbb5ace896804a164810622b63e553d2d5f1e70b
Viicos <victo.plot@gmail.com>
2022-09-13 10:25:07 +0200

    :sparkles: [#5] -- Add viewset for `Proof`

--- src/bdeapp/api/serializers.py
+++ src/bdeapp/api/serializers.py
@@ -1,29 +1,32 @@
-
 from rest_framework import serializers
 
-from bdeapp.challenges.models import Challenge, FamilyStatus
-from bdeapp.families.models import Family
+from bdeapp.challenges.models import Challenge, FamilyStatus, Proof
 from bdeapp.events.models import Event
+from bdeapp.families.models import Family
+
 
 class EventSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Event
-        fields = ["uuid", "published", "name", "date", "time", "description", "long_description"]
+        fields = [
+            "uuid",
+            "published",
+            "name",
+            "date",
+            "time",
+            "description",
+            "long_description",
+        ]
 
 
 class FamilySerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Family
         fields = ["uuid", "name", "image"]
 
 
 class FamilyStatusSerializer(serializers.ModelSerializer):
-    family = serializers.SlugRelatedField(
-        slug_field="uuid",
-        read_only=True
-    )
+    family = serializers.SlugRelatedField(slug_field="uuid", read_only=True)
 
     class Meta:
         model = FamilyStatus
@@ -35,4 +38,24 @@ class ChallengeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Challenge
-        fields = ["uuid", "name", "description", "points", "max_validations", "families_status"]
+        fields = [
+            "uuid",
+            "name",
+            "description",
+            "points",
+            "max_validations",
+            "families_status",
+        ]
+
+
+class ProofSerializer(serializers.ModelSerializer):
+    family = serializers.SlugRelatedField(
+        slug_field="uuid", queryset=Family.objects.all()
+    )
+    challenge = serializers.SlugRelatedField(
+        slug_field="uuid", queryset=Challenge.objects.all()
+    )
+
+    class Meta:
+        model = Proof
+        fields = ["uuid", "family", "challenge", "name", "proof_content"]
--- src/bdeapp/api/views.py
+++ src/bdeapp/api/views.py
@@ -1,26 +1,37 @@
+from rest_framework import mixins, viewsets
 
-from rest_framework import viewsets
-
+from bdeapp.challenges.models import Challenge, Proof
 from bdeapp.events.models import Event
-from bdeapp.challenges.models import Challenge
 from bdeapp.families.models import Family
 
-from .serializers import EventSerializer, ChallengeSerializer, FamilySerializer
+from .serializers import (
+    ChallengeSerializer,
+    EventSerializer,
+    FamilySerializer,
+    ProofSerializer,
+)
 
 
-class EventViewSet(viewsets.ModelViewSet):
+class EventViewSet(viewsets.ReadOnlyModelViewSet):
     lookup_field = "uuid"
     queryset = Event.objects.filter(published=True)
     serializer_class = EventSerializer
 
 
-class FamilyViewSet(viewsets.ModelViewSet):
+class FamilyViewSet(viewsets.ReadOnlyModelViewSet):
     lookup_field = "uuid"
     queryset = Family.objects.all()
     serializer_class = FamilySerializer
 
 
-class ChallengeViewSet(viewsets.ModelViewSet):
+class ChallengeViewSet(viewsets.ReadOnlyModelViewSet):
     lookup_field = "uuid"
     queryset = Challenge.objects.all()
     serializer_class = ChallengeSerializer
+
+
+class ProofViewSet(mixins.CreateModelMixin, viewsets.ReadOnlyModelViewSet):
+    permission_classes = []
+    lookup_field = "uuid"
+    queryset = Proof.objects.filter(published=True)
+    serializer_class = ProofSerializer
